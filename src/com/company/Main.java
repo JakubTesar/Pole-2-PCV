ce3b24f96f2bba0f4e93fb7e95e9204d715368d6
JakubTesar <jakub.tesar@educanet.cz>
2020-12-14 10:23:38 +0100

    pls

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,24 +16,20 @@ public class Main {
             pole2.add(random.nextInt(100));
         }
 
-        for (int i = 0; i <150; i++) {
-            if (pole1.get(i) == pole2.get(i)){
-                System.out.println("Ano jsou stejné");
-            } else {
-                System.out.println("Ne nejsou stejné");
-            }
-
+        for (int i = 50; i < 100; i++) {
+            pole1.remove(i);
         }
 
-
-        for (int i = 0; i < pole1.size(); i++) {
+        for (int i = 0; i < 100; i++) {
             pole2.set(i, pole1.get(i));
         }
+
         if (pole1 == pole2){
             System.out.println("Ano jsou stejné");
         } else {
             System.out.println("Ne nejsou stejné");
         }
-
+        System.out.println(pole1);
+        System.out.println(pole2);
     }
 }
